117616ca2d05af66ca6ab88fef9603358a8722fc
fadedreams7 <fadedreams7@gmail.com>
2024-06-15 05:21:55 +0000

    acc_test crud tests+

--- foundation/db/sqlc/acc_test.go
+++ foundation/db/sqlc/acc_test.go
@@ -2,9 +2,8 @@ package db
 
 import (
 	"context"
-	// "database/sql"
 	"testing"
-	// "time"
+	"time"
 
 	"github.com/fadedreams/gofinanceflow/foundation/sdk"
 	"github.com/stretchr/testify/require"
@@ -36,48 +35,49 @@ func TestCreateAccount(t *testing.T) {
 	createRandomAccount(t)
 }
 
-// func TestGetAccount(t *testing.T) {
-// 	account1 := createRandomAccount(t)
-// 	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
-// 	require.NoError(t, err)
-// 	require.NotEmpty(t, account2)
-//
-// 	require.Equal(t, account1.ID, account2.ID)
-// 	require.Equal(t, account1.Owner, account2.Owner)
-// 	require.Equal(t, account1.Balance, account2.Balance)
-// 	require.Equal(t, account1.Currency, account2.Currency)
-// 	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
-// }
-//
-// func TestUpdateAccount(t *testing.T) {
-// 	account1 := createRandomAccount(t)
-//
-// 	arg := UpdateAccountParams{
-// 		ID:      account1.ID,
-// 		Balance: sdk.RandomMoney(),
-// 	}
-//
-// 	account2, err := testQueries.UpdateAccount(context.Background(), arg)
-// 	require.NoError(t, err)
-// 	require.NotEmpty(t, account2)
-//
-// 	require.Equal(t, account1.ID, account2.ID)
-// 	require.Equal(t, account1.Owner, account2.Owner)
-// 	require.Equal(t, arg.Balance, account2.Balance)
-// 	require.Equal(t, account1.Currency, account2.Currency)
-// 	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
-// }
-//
-// func TestDeleteAccount(t *testing.T) {
-// 	account1 := createRandomAccount(t)
-// 	err := testQueries.DeleteAccount(context.Background(), account1.ID)
-// 	require.NoError(t, err)
-//
-// 	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
-// 	require.Error(t, err)
-// 	require.EqualError(t, err, sql.ErrNoRows.Error())
-// 	require.Empty(t, account2)
-// }
+func TestGetAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+}
+
+func TestUpdateAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+
+	arg := UpdateAccountParams{
+		ID:      account1.ID,
+		Balance: sdk.RandomMoney(),
+	}
+
+	account2, err := testQueries.UpdateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, arg.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+}
+
+func TestDeleteAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+	err := testQueries.DeleteAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "no rows in result set")
+	require.Empty(t, account2)
+}
+
 //
 // func TestListAccounts(t *testing.T) {
 // 	for i := 0; i < 10; i++ {
